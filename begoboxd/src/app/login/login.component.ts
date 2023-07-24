d38ea40076d6d66e95293cc2c277a4fb10987356
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-07-24 16:10:23 +0300

    login page & header implemented

--- begoboxd/src/app/login/login.component.ts
+++ begoboxd/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username!: string;
+  password!: string;
+
+  onSubmit() {
+    console.log('Username: ' + this.username);
+    console.log('Password: ' + this.password);
+  }
 
 }
