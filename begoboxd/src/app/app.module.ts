74fcceacacd7daa949c6300cc3b5c2b2d5e3ab14
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2023-07-26 10:59:30 +0300

    footer & about components created

--- begoboxd/src/app/app.module.ts
+++ begoboxd/src/app/app.module.ts
@@ -1,18 +1,26 @@
+//modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from "@angular/forms";
+
+//components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
